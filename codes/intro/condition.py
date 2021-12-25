c187109d6cf6e308541875cb7c2a158cefe78300
Vaibhav Ahuja <19bcs1065@cuchd.in>
2021-12-25 22:46:49 +0530

    conditional statements

---
+++ codes/intro/condition.py
@@ -0,0 +1,13 @@
+#print(5<"1") #type error
+print(1 == "1") # Flase as one of the is string and other is a integer
+#logical operators
+# and not or
+print(1==1 and 2==1)
+print(1==1 or 2==1)
+a = "vaibhav"
+if len(a)<3:
+    print("invalid")
+elif len(a)>8:
+    print("invalid")
+else:
+    print("valid")
